3a7add6196e104f0c4c064acb999c3c033316b51
katrinedelrosario <99542678+katrinedelrosario@users.noreply.github.com>
2022-08-18 10:59:50 +0200

    added comments

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ const myHeading = document.createElement('h1');     //creating element
 myHeading.innerText = 'hellow world';
 myApp.appendChild(myHeading);                       //getting parent-element first then connects it with child-element in the ()
 
-
-const myFig = document.createElement('figure');
+                                                    //creating elements
+const myFig = document.createElement('figure');     
 const myH = document.createElement('h2');
 const myImg = document.createElement('img');
 const myP = document.createElement('p');
-
+                                                    //connecting elements to parent-elements as child
 myApp.appendChild(myFig);
 myFig.appendChild(myH);
 myFig.appendChild(myImg);
 myFig.appendChild(myP);
-myImg.src = 'js/assets/images/little-me.jpg';
-
+myImg.src = 'js/assets/images/little-me.jpg';       //linking image
+                                                    //adding text to elements
 myH.innerText = 'mellow me';
 myP.innerText = 'this is me n im cool actually';
 
